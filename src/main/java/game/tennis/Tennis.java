b74bfe6519fc7c1b16523b9c083a1c45f9ef2a17
Akshay Patil <akshay.patil4397@yahoo.com>
2022-03-15 09:37:18 +0530

    updated winning point constant

--- src/main/java/game/tennis/Tennis.java
+++ src/main/java/game/tennis/Tennis.java
@@ -33,8 +33,8 @@ public class Tennis {
 
         while (Objects.isNull(this.scoreBoard.getWinner())) {
 
-            updateCurrentPlayer();
             int points = helperUtils.generateRandomPoint();
+            updateCurrentPlayer();
             settleResult(points);
 
         }
